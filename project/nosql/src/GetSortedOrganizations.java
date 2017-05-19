b646064b214653f76786e8e6b52aa01e5742ff6d
Theodore Bigelow <meliornox@gmail.com>
2017-05-18 21:35:54 -0400

    Update GetSortedOrganizations.java

--- project/nosql/src/GetSortedOrganizations.java
+++ project/nosql/src/GetSortedOrganizations.java
@@ -2,29 +2,58 @@
  * Created by jgb23 on 5/13/2017.
  * This is useful to get organizations sorted by budget to determine where the college can cut costs.
  */
-import oracle.kv.*;
-import oracle.kv.impl.fault.SystemFaultException;
-
-import java.sql.SQLException;
-import java.util.*;
-
-public class GetSortedParticipants {
-    public static void main(String[] args) throws SQLException {
-        GetSortedOrganizations so = new GetSortedOrganizations();
-        so.run();
-    }
-
-    public void run() {
-        KVStore store = KVStoreFactory.getStore(new KVStoreConfig("kvstore", "localhost:5000"));
-
-        Key key = Key.createKey("organization");
-
-        Map<Key, ValueVersion> fields = store.multiGet(majorKeyPathOnly, null, null);
-        for (Map.Entry<Key, ValueVersion> field : fields.entrySet()) {
-            String value = new String(field.getValue().getValue().getValue());
-            System.out.println(value);
-        }
-
-        store.close();
-    }
-}
+ import oracle.kv.*;
+ 
+ import java.sql.Connection;
+ import java.sql.DriverManager;
+ import java.sql.SQLException;
+ import java.util.*;
+ 
+ public class GetSortedOrganizations {
+ 
+     public static void main(String[] args) throws SQLException {
+         Connection jdbcConnection = DriverManager.getConnection(
+                 "jdbc:oracle:thin:@localhost:1521:xe", "events", "bjarne");
+         execute(jdbcConnection);
+     }
+ 
+     public static void execute(Connection connection) throws SQLException {
+         KVStore store = KVStoreFactory.getStore(new KVStoreConfig("kvstore", "localhost:5000"));
+ 
+         LoadDB.loadOrganizations(store, connection);
+ 
+         List<Organization> organizations = new ArrayList<Organization>();
+         Key key = Key.createKey(Arrays.asList("organization"));
+ 
+         String name = "";
+         String budget = "";
+         String orgId= "";
+ 
+         Map<Key, ValueVersion> fields = store.multiGet(key, null, null);
+ 
+         for (Map.Entry<Key, ValueVersion> field : fields.entrySet()) {
+             orgId = field.getKey().getMinorPath().get(0);
+             if(field.getKey().getMinorPath().get(1).equals("name")) {
+                 name = new String(field.getValue().getValue().getValue());
+             }
+             if(field.getKey().getMinorPath().get(1).equals("budget")) {
+                 budget = new String(field.getValue().getValue().getValue());
+             }
+             if(budget != "" ) {
+                 Organization org = new Organization(Float.parseFloat(budget), name, orgId);
+                 organizations.add(org);
+             }
+         }
+ 
+         Collections.sort(organizations, new Comparator<Organization>() {
+             @Override
+             public int compare(Organization o1, Organization o2) {
+                 return  Math.round(o1.getBudget() - o2.getBudget());
+             }
+         });
+ 
+         for(int i=0; i< organizations.size(); i++){
+             System.out.println(organizations.get(i).getBudget() + "\t" + organizations.get(i).getId() + "\t" + organizations.get(i).getName() );
+         }
+     }
+ } 
